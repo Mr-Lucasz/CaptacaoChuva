2f1b5440dd6dc9a3a63d5a83ee9763a300ef7539
Lucas Rodrigues <84003586+Mr-Lucasz@users.noreply.github.com>
2022-02-14 23:08:56 -0300

    Update Telhado.java

--- trabalhofinal/Telhado.java
+++ trabalhofinal/Telhado.java
@@ -4,36 +4,67 @@
  */
 package trabalhofinal;
 
-/**
- *
- * @author LRodrigues
- */
-public class Telhado {
+
+import java.util.ArrayList;
+
+public class Telhado{
    
     private double comprimento;
     private double largura;
-
-    public Telhado(double comprimento, double largura) {
-        this.comprimento = comprimento;
-        this.largura = largura;
-    }
-
-    public double getComprimento() {
-        return comprimento;
-    }
-
-    public void setComprimento(double comprimento) {
-        this.comprimento = comprimento;
-    }
-
-    public double getLargura() {
-        return largura;
-    }
-
-    public void setLargura(double largura) {
-        this.largura = largura;
-    }
-
+    private ArrayList<Chuva> quantChuva;
     
+    public Telhado(double comprimento, double largura){
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.quantChuva = new ArrayList<Chuva>();
+    }
     
+    public void addChuva(Chuva chu){
+        this.quantChuva.add(chu);
+    }
+    
+    public Chuva getChuva(){
+        for(int i = 0; i < this.quantChuva.size(); i++){
+            Chuva chu = this.quantChuva.get(i);
+            return chu;
+        }return null;
+    }
+    
+    public void imprimirChuvaDia(){
+        System.out.println("Quantidade de chuva armazenada: \n");
+        for(Chuva chu : this.quantChuva){
+            System.out.println(chu.toString() + "\n");
+        }
+    }
+    
+    public boolean setComprimento(double comprimento){
+        if(comprimento > 0){
+            this.comprimento = comprimento;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public double getComprimento(){
+        return this.comprimento;
+    }
+    
+    public boolean setLargura(double largura){
+        if(largura > 0){
+            this.largura = largura;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public double getLargura(){
+        return this.largura;
+    }
+    
+    public String toString(){
+        String chuvaTelhado = "O telhado tem o volume de: "+(this.largura * this.comprimento);
+        return chuvaTelhado;
+    }
 }
